afc597dbe12704c2ef0add3df1ff4b16c7c69d3a
af8a2a <1125209282@qq.com>
2024-02-09 19:11:37 +0800

    async benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -4,54 +4,65 @@ use piggysql::{
     storage::{engine::memory::Memory, MVCCLayer},
 };
 
-// fn generate_random_string(length: usize) -> String {
-//     let rng = thread_rng();
-//     let mut random_string: Vec<u8> = rng.sample_iter(&Alphanumeric).take(length).collect();
-//     String::from_utf8(random_string).expect("error")
-// }
-lazy_static! {
-    static ref DATA: Database<MVCCLayer<Memory>> = data_source().unwrap();
-}
 use anyhow::Result;
-use lazy_static::lazy_static;
 
-fn data_source() -> Result<Database<MVCCLayer<Memory>>> {
+async fn data_source() -> Result<Database<MVCCLayer<Memory>>> {
     let db = Database::new(MVCCLayer::new(Memory::new()))?;
+
     db.run(
         "CREATE TABLE BenchTable(
-        id INT PRIMARY KEY,
-        val INT);
-        ",
-    )?;
+            id INT PRIMARY KEY,
+            val INT);
+            ",
+    )
+    .await?;
     for i in 0..100000 {
         db.run(&format!("INSERT INTO BenchTable VALUES ({},{})", i, i,))
+            .await
             .expect("insert error");
     }
+
     Ok(db)
 }
 
-
-pub fn primary_key_benchmark_100000() {
-    let engine = &DATA;
-    
+pub async fn primary_key_benchmark_100000(engine: &Database<MVCCLayer<Memory>>) -> Result<()> {
     let _ = engine
-        .run("SELECT * FROM BenchTable where id>50000")
-        .unwrap();
+        .run("SELECT * FROM BenchTable where id>90000")
+        .await?;
+    Ok(())
 }
-pub fn without_primary_key_benchmark_100000() {
-    let engine = &DATA;
-
+pub async fn without_primary_key_benchmark_100000(
+    engine: &Database<MVCCLayer<Memory>>,
+) -> Result<()> {
     let _ = engine
-        .run("SELECT * FROM BenchTable where val>50000")
-        .unwrap();
+        .run("SELECT * FROM BenchTable where val>90000")
+        .await?;
+    Ok(())
 }
+
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("select in 1000 rows with primary key", |b| {
-        b.iter(|| primary_key_benchmark_100000())
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(8)
+        .enable_all()
+        .build()
+        .unwrap();
+    let engine = rt.block_on(async { data_source().await.unwrap() });
+
+    c.bench_function("select rows with primary key", |b| {
+        b.to_async(&rt)
+            .iter(|| primary_key_benchmark_100000(&engine))
     });
-    c.bench_function("select in 1000 rows without primary key", |b| {
-        b.iter(|| without_primary_key_benchmark_100000())
+    c.bench_function("select rows without primary key", |b| {
+        b.to_async(&rt)
+            .iter(|| without_primary_key_benchmark_100000(&engine))
     });
 }
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(
+    name = benches;
+    config = Criterion::default()
+    .sample_size(10)
+    .measurement_time(std::time::Duration::from_secs(30))
+    ;
+    targets = criterion_benchmark
+);
 criterion_main!(benches);
