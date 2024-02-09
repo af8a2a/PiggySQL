3c2dee11a995946907139c0ee2e1dbcdb4e79cb9
af8a2a <1125209282@qq.com>
2024-02-09 21:40:57 +0800

    wip refractor

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,4 @@
 
-use std::sync::Arc;
-
 use sqlparser::ast::Statement;
 use sqlparser::parser::ParserError;
 
@@ -8,9 +6,6 @@ use crate::binder::{BindError, Binder, BinderContext};
 
 use crate::execution::executor::{build, BoxedExecutor};
 use crate::execution::ExecutorError;
-
-
-
 
 use crate::optimizer::{apply_optimization, OptimizerError};
 use crate::parser;
@@ -20,71 +15,6 @@ use crate::types::tuple::Tuple;
 
 pub struct Database<S: Storage> {
     pub(crate) storage: S,
-}
-
-pub struct Session<S: Storage> {
-    /// The underlying store
-    db: Arc<Database<S>>,
-    /// The current session transaction, if any
-    txn: Option<DBTransaction<S>>,
-}
-impl<S: Storage> Session<S> {
-    pub fn new(db: Arc<Database<S>>) -> Self {
-        Session { db, txn: None }
-    }
-
-    pub fn execute(&mut self, query: &str) -> Result<Vec<Tuple>, DatabaseError> {
-        let stmts = parser::parse(query).unwrap();
-        if stmts.is_empty() {
-            return Err(DatabaseError::EmptyStatement);
-        }
-        let mut tuple = Vec::new();
-        let stmt = &stmts[0];
-        match stmt {
-            Statement::StartTransaction { .. } if self.txn.is_some() => {
-                return Err(DatabaseError::InternalError(
-                    "Already in a transaction".into(),
-                ));
-            }
-            Statement::StartTransaction { .. } => {
-                let txn = self.db.new_transaction()?;
-                self.txn = Some(txn);
-            }
-            Statement::Commit { .. } | Statement::Rollback { .. } if self.txn.is_none() => {
-                return Err(DatabaseError::InternalError("Not in a transaction".into()));
-            }
-
-            Statement::Rollback { .. } => {
-                let txn = self.txn.take().unwrap();
-                txn.rollback()?;
-            }
-            Statement::Commit { .. } => {
-                let txn = self.txn.take().unwrap();
-                txn.commit()?;
-            }
-            //already in a transaction
-            statement if self.txn.is_some() => {
-                if stmts.is_empty() {
-                    return Err(DatabaseError::EmptyStatement);
-                }
-                if let Some(txn) = &mut self.txn {
-                    tuple = txn.run_with_stmt(statement)?;
-                }
-            }
-            //DQL
-            statement @ Statement::Query(..) => {
-                let mut txn = self.db.new_transaction()?;
-                tuple = txn.run_with_stmt(statement)?;
-                txn.rollback()?;
-            }
-            statement => {
-                let mut txn = self.db.new_transaction()?;
-                tuple = txn.run_with_stmt(statement)?;
-                txn.commit()?;
-            }
-        };
-        Ok(tuple)
-    }
 }
 
 impl<S: Storage> Database<S> {
@@ -106,9 +36,7 @@ impl<S: Storage> Database<S> {
     pub fn new_transaction(&self) -> Result<DBTransaction<S>, DatabaseError> {
         let transaction = self.storage.transaction()?;
 
-        Ok(DBTransaction {
-            inner: transaction,
-        })
+        Ok(DBTransaction { inner: transaction })
     }
 
     fn _run(
@@ -129,37 +57,7 @@ impl<S: Storage> Database<S> {
         Ok(build(best_plan, transaction))
     }
 
-    // pub fn default_optimizer(source_plan: LogicalPlan) -> HepOptimizer {
-    //     HepOptimizer::new(source_plan)
-    //         .batch(
-    //             "Column Pruning".to_string(),
-    //             HepBatchStrategy::once_topdown(),
-    //             vec![RuleImpl::ColumnPruning],
-    //         )
-    //         .batch(
-    //             "Simplify Filter".to_string(),
-    //             HepBatchStrategy::fix_point_topdown(10),
-    //             vec![RuleImpl::SimplifyFilter, RuleImpl::ConstantFolder],
-    //         )
-    //         .batch(
-    //             "Predicate Pushdown".to_string(),
-    //             HepBatchStrategy::fix_point_topdown(10),
-    //             vec![
-    //                 RuleImpl::PushPredicateThroughJoin,
-    //                   RuleImpl::PushPredicateIntoScan,
-    //             ],
-    //         )
-    //         .batch(
-    //             "Combine Operators".to_string(),
-    //             HepBatchStrategy::fix_point_topdown(10),
-    //             vec![RuleImpl::CollapseProject, RuleImpl::CombineFilter],
-    //         )
-    //         .batch(
-    //             "Limit Pushdown".to_string(),
-    //             HepBatchStrategy::fix_point_topdown(10),
-    //             vec![RuleImpl::PushLimitIntoTableScan],
-    //         )
-    // }
+
 }
 
 pub struct DBTransaction<S: Storage> {
@@ -245,7 +143,6 @@ mod test {
     use std::sync::Arc;
     use tempfile::TempDir;
 
-    use super::Session;
 
     fn build_table(mut transaction: impl Transaction) -> Result<(), StorageError> {
         let columns = vec![
@@ -268,33 +165,6 @@ mod test {
         Ok(())
     }
 
-    #[tokio::test]
-    async fn test_run_sql() -> Result<(), DatabaseError> {
-        let database = Arc::new(Database::new(MVCCLayer::new(Memory::new()))?);
-        let mut session = Session::new(database.clone());
-        session.execute("begin")?;
-        session.execute("create table t1 (a int primary key, b int)")?;
-        session.execute("commit")?;
-        session.execute("begin")?;
-        session.execute("insert into t1 values(2, 2)")?;
-        let tuple = session.execute("select * from t1")?;
-        assert_eq!(
-            tuple[0].values,
-            vec![
-                Arc::new(DataValue::Int32(Some(2))),
-                Arc::new(DataValue::Int32(Some(2)))
-            ]
-        );
-        session.execute("rollback")?;
-        session.execute("begin")?;
-
-        let tuple = session.execute("select * from t1")?;
-        session.execute("commit")?;
-
-        assert_eq!(tuple, vec![]);
-
-        Ok(())
-    }
 
     #[tokio::test]
     async fn test_transaction_sql() -> Result<(), DatabaseError> {
@@ -304,14 +174,14 @@ mod test {
 
         let _ = tx_1.run("create table t1 (a int primary key, b int)")?;
         tx_1.run("create index test_index on t1 (b)")?;
-        let tuples=tx_1.run("explain select * from t1 where b>1")?;
-        for tuple in tuples{
-            println!("{}",tuple);
+        let tuples = tx_1.run("explain select * from t1 where b>1")?;
+        for tuple in tuples {
+            println!("{}", tuple);
         }
         tx_1.run("drop index t1.test_index")?;
-        let tuples=tx_1.run("explain select * from t1 where b>1")?;
-        for tuple in tuples{
-            println!("{}",tuple);
+        let tuples = tx_1.run("explain select * from t1 where b>1")?;
+        for tuple in tuples {
+            println!("{}", tuple);
         }
 
         // assert_eq!(
@@ -340,13 +210,20 @@ mod test {
         let _ = database.run(
             "create table t1 (a int primary key, b int unique null, k int, z varchar unique null)",
         ).await?;
-        let _ =
-            database.run("create table t2 (c int primary key, d int unsigned null, e datetime)").await?;
+        let _ = database
+            .run("create table t2 (c int primary key, d int unsigned null, e datetime)")
+            .await?;
         let _ = database.run("insert into t1 (a, b, k, z) values (-99, 1, 1, 'k'), (-1, 2, 2, 'i'), (5, 3, 2, 'p'), (29, 4, 2, 'db')").await?;
         let _ = database.run("insert into t2 (d, c, e) values (2, 1, '2021-05-20 21:00:00'), (3, 4, '2023-09-10 00:00:00')").await?;
-        let _ = database.run("create table t3 (a int primary key, b decimal(4,2))").await?;
-        let _ = database.run("insert into t3 (a, b) values (1, 1111), (2, 2.01), (3, 3.00)").await?;
-        let _ = database.run("insert into t3 (a, b) values (4, 4444), (5, 5222), (6, 1.00)").await?;
+        let _ = database
+            .run("create table t3 (a int primary key, b decimal(4,2))")
+            .await?;
+        let _ = database
+            .run("insert into t3 (a, b) values (1, 1111), (2, 2.01), (3, 3.00)")
+            .await?;
+        let _ = database
+            .run("insert into t3 (a, b) values (4, 4444), (5, 5222), (6, 1.00)")
+            .await?;
 
         println!("full t1:");
         let tuples_full_fields_t1 = database.run("select * from t1").await?;
@@ -374,7 +251,9 @@ mod test {
         println!("{}", create_table(&tuples_like_2_t1));
 
         println!("not like t1:");
-        let tuples_not_like_t1 = database.run("select * from t1 where z not like '_b'").await?;
+        let tuples_not_like_t1 = database
+            .run("select * from t1 where z not like '_b'")
+            .await?;
         println!("{}", create_table(&tuples_not_like_t1));
 
         println!("in t1:");
@@ -382,7 +261,9 @@ mod test {
         println!("{}", create_table(&tuples_in_t1));
 
         println!("not in t1:");
-        let tuples_not_in_t1 = database.run("select * from t1 where a not in (5, 29)").await?;
+        let tuples_not_in_t1 = database
+            .run("select * from t1 where a not in (5, 29)")
+            .await?;
         println!("{}", create_table(&tuples_not_in_t1));
 
         println!("limit:");
@@ -390,19 +271,27 @@ mod test {
         println!("{}", create_table(&tuples_limit));
 
         println!("inner join:");
-        let tuples_inner_join = database.run("select * from t1 inner join t2 on a = c").await?;
+        let tuples_inner_join = database
+            .run("select * from t1 inner join t2 on a = c")
+            .await?;
         println!("{}", create_table(&tuples_inner_join));
 
         println!("left join:");
-        let tuples_left_join = database.run("select * from t1 left join t2 on a = c").await?;
+        let tuples_left_join = database
+            .run("select * from t1 left join t2 on a = c")
+            .await?;
         println!("{}", create_table(&tuples_left_join));
 
         println!("right join:");
-        let tuples_right_join = database.run("select * from t1 right join t2 on a = c").await?;
+        let tuples_right_join = database
+            .run("select * from t1 right join t2 on a = c")
+            .await?;
         println!("{}", create_table(&tuples_right_join));
 
         println!("full join:");
-        let tuples_full_join = database.run("select * from t1 full join t2 on a = c").await?;
+        let tuples_full_join = database
+            .run("select * from t1 full join t2 on a = c")
+            .await?;
         println!("{}", create_table(&tuples_full_join));
 
         println!("count agg:");
@@ -434,7 +323,9 @@ mod test {
         println!("{}", create_table(&tuples_min_max_agg));
 
         println!("group agg:");
-        let tuples_group_agg = database.run("select c, max(d) from t2 group by c having c = 1").await?;
+        let tuples_group_agg = database
+            .run("select c, max(d) from t2 group by c having c = 1")
+            .await?;
         println!("{}", create_table(&tuples_group_agg));
 
         println!("alias:");
@@ -442,8 +333,9 @@ mod test {
         println!("{}", create_table(&tuples_group_agg));
 
         println!("alias agg:");
-        let tuples_group_agg =
-            database.run("select c, max(d) as max_d from t2 group by c having c = 1").await?;
+        let tuples_group_agg = database
+            .run("select c, max(d) as max_d from t2 group by c having c = 1")
+            .await?;
         println!("{}", create_table(&tuples_group_agg));
 
         println!("time max:");
@@ -451,10 +343,15 @@ mod test {
         println!("{}", create_table(&tuples_time_max));
 
         println!("time where:");
-        let tuples_time_where_t2 = database.run("select (c + 1) from t2 where e > '2021-05-20'").await?;
+        let tuples_time_where_t2 = database
+            .run("select (c + 1) from t2 where e > '2021-05-20'")
+            .await?;
         println!("{}", create_table(&tuples_time_where_t2));
 
-        assert!(database.run("select max(d) from t2 group by c").await.is_err());
+        assert!(database
+            .run("select max(d) from t2 group by c")
+            .await
+            .is_err());
 
         println!("distinct t1:");
         let tuples_distinct_t1 = database.run("select distinct b, k from t1").await?;
@@ -467,13 +364,16 @@ mod test {
         println!("{}", create_table(&update_after_full_t1));
 
         println!("insert overwrite t1:");
-        let _ = database.run("insert overwrite t1 (a, b, k) values (-99, 1, 0)").await?;
+        let _ = database
+            .run("insert overwrite t1 (a, b, k) values (-99, 1, 0)")
+            .await?;
         println!("after t1:");
         let insert_overwrite_after_full_t1 = database.run("select * from t1").await?;
         println!("{}", create_table(&insert_overwrite_after_full_t1));
 
         assert!(database
-            .run("insert overwrite t1 (a, b, k) values (-1, 1, 0)").await
+            .run("insert overwrite t1 (a, b, k) values (-1, 1, 0)")
+            .await
             .is_err());
 
         println!("delete t1 with filter:");
