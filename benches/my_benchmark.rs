00b5807b8a5b5179352978e5d8b2d4a893ec08a7
af8a2a <1125209282@qq.com>
2024-02-27 12:00:24 +0800

    fix bug

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -2,8 +2,12 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use piggysql::{
     db::Database,
     errors::*,
-    storage::{engine::memory::Memory, MVCCLayer},
+    storage::{
+        engine::{lsm::BitCask, memory::Memory},
+        MVCCLayer,
+    },
 };
+use tempfile::{tempdir, tempdir_in};
 
 async fn data_source() -> Result<Database<MVCCLayer<Memory>>> {
     let db = Database::new(MVCCLayer::new(Memory::new()))?;
@@ -26,7 +30,31 @@ async fn data_source() -> Result<Database<MVCCLayer<Memory>>> {
         .await?;
     Ok(db)
 }
+async fn data_source_bitcask() -> Result<Database<MVCCLayer<BitCask>>> {
+    let path = tempdir::TempDir::new("piggydb")
+        .unwrap()
+        .path()
+        .join("piggydb");
+    let db = Database::new(MVCCLayer::new(BitCask::new(path)?))?;
 
+    db.run(
+        "CREATE TABLE BenchTable(
+            id INT PRIMARY KEY,
+            val INT);
+            ",
+    )
+    .await?;
+    let mut batch = String::new();
+    for i in 0..100000 {
+        batch += format!("({},{})", i, i).as_str();
+        batch += ","
+    }
+    batch += format!("({},{})", 100001, 100001).as_str();
+
+    db.run(&format!("INSERT INTO BenchTable VALUES {}", batch))
+        .await?;
+    Ok(db)
+}
 pub async fn primary_key_benchmark_100000(engine: &Database<MVCCLayer<Memory>>) -> Result<()> {
     let _ = engine
         .run("SELECT * FROM BenchTable where id=90000")
@@ -53,6 +81,13 @@ pub async fn ssi_key_benchmark_100000(engine: &Database<MVCCLayer<Memory>>) -> R
     let _ = txn.run("SELECT * FROM BenchTable where val=90000").await?;
     Ok(())
 }
+
+pub async fn bitcask_benchmark_100000(engine: &Database<MVCCLayer<BitCask>>) -> Result<()> {
+    let _ = engine
+        .run("SELECT * FROM BenchTable where val=90000")
+        .await?;
+    Ok(())
+}
 fn criterion_benchmark(c: &mut Criterion) {
     let rt = tokio::runtime::Builder::new_multi_thread()
         .worker_threads(8)
@@ -60,21 +95,32 @@ fn criterion_benchmark(c: &mut Criterion) {
         .build()
         .unwrap();
     let engine = rt.block_on(async { data_source().await.unwrap() });
-
+    let bitcask = rt.block_on(async { data_source_bitcask().await.unwrap() });
     c.bench_function("select rows with primary key", |b| {
         b.to_async(&rt)
             .iter(|| async { primary_key_benchmark_100000(&engine).await })
     });
     c.bench_function("select rows without primary key", |b| {
         b.to_async(&rt)
-            .iter(||async {without_primary_key_benchmark_100000(&engine).await})
+            .iter(|| async { without_primary_key_benchmark_100000(&engine).await })
     });
     c.bench_function("transaction in Snapshot isolation", |b| {
-        b.to_async(&rt).iter(|| async {si_key_benchmark_100000(&engine).await})
+        b.to_async(&rt)
+            .iter(|| async { si_key_benchmark_100000(&engine).await })
     });
     c.bench_function("transaction in Serializable Snapshot isolation", |b| {
-        b.to_async(&rt).iter(||async {ssi_key_benchmark_100000(&engine).await})
+        b.to_async(&rt)
+            .iter(|| async { ssi_key_benchmark_100000(&engine).await })
     });
+    c.bench_function("bitcask benchmark select rows with primary key", |b| {
+        b.to_async(&rt)
+            .iter(|| async { bitcask_benchmark_100000(&bitcask).await })
+    });
+    c.bench_function("bitcask benchmark select rows with primary key", |b| {
+        b.to_async(&rt)
+            .iter(|| async { bitcask_benchmark_100000(&bitcask).await })
+    });
+
 }
 criterion_group!(
     name = benches;
