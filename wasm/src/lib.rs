0566bdfc6e0022ba20bbcc26511dc98fadd1958c
af8a2a <1125209282@qq.com>
2024-03-03 15:09:40 +0800

    wip: simple shell

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -19,7 +19,6 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
 
