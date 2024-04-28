e1f3ebb76ffbe6140eab77d297610e4bc2ab2dbf
af8a2a <1125209282@qq.com>
2024-04-28 13:13:02 +0800

    update client

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,6 +7,7 @@ use rustyline::DefaultEditor;
 async fn main() -> Result<()> {
     // `()` can be used when no completer is required
     let mut rl = DefaultEditor::new()?;
+    println!("Ctrl-C to exit, .exit to exit, .help to get help");
     if rl.load_history("history.txt").is_err() {
         println!("No previous history.");
     }
@@ -16,21 +17,36 @@ async fn main() -> Result<()> {
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str())?;
-                match client.query(line.as_str()).await {
-                    Ok(result) => println!("{}", create_table(result)),
-                    Err(e) => {
-                        println!("Error: {:?}", e);
+                match line.as_str() {
+                    ".exit" => break,
+                    ".help"|".h"=>{
+                        println!("show tables: show all tables");
+                        println!("describe [table]: describe table schema");
+                        println!("explain [sql]: explain sql");
+                        println!("type Postgre dilect SQL to query");
+                    }
+                    _=>match client.query(line.as_str()).await {
+                        Ok(result) => println!("{}", create_table(result)),
+                        Err(e) => {
+                            println!("Error: {:?}", e);
+                        }
                     }
                 }
+                // match client.query(line.as_str()).await {
+                //     Ok(result) => println!("{}", create_table(result)),
+                //     Err(e) => {
+                //         println!("Error: {:?}", e);
+                //     }
+                // }
                 // let result = client.query(line.as_str()).await?;
                 // println!("{}", create_table(result));
             }
             Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
+                println!("exit by Ctrl-C");
                 break;
             }
             Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
+                println!("exit by Ctrl-D");
                 break;
             }
             Err(err) => {
